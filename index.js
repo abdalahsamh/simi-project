fcccb368a504ee4b86c9ce206a431c99f3ad5cb3
abdalahsamh <https://github.com/abdalahsamh>
2025-02-10 11:36:35 +0200

    2-10-2025

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let carLink = document.querySelector(".car-link");
+// document.addEventListener("DOMContentLoaded", function () {
+// //   let carLink = document.querySelector(".car-link");
 
-  let observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        }
-      });
-    },
-    { threshold: 0.7 }
-  );
+//   let observer = new IntersectionObserver(
+//     (entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add("show");
+//         }
+//       });
+//     },
+//     { threshold: 0.7 }
+//   );
 
-  observer.observe(carLink);
-});
+//   observer.observe(carLink);
+// });
